6ccd4ebe998a1c8a10b34e16ec5d75b13099c19f
Jackmc <jackmc1122@gmail.com>
2019-05-21 11:33:16 -0700

    Add imports for middlewares and controllers and add post requests for login resister and profile/id.

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const cors = require("cors");
 // const knex = require("knex");
 // const jwt = require("jsonwebtoken");
 
+const register = require("./controllers/register");
+const login = require("./controllers/login");
+const profile = require("./controllers/profile");
+const auth = require("./middlewares/authorization");
+
 const app = express();
 
 app.use(cors());
@@ -12,6 +17,16 @@ app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("alchemy type is working");
+});
+
+app.post("/login", (req, res) => {
+  login.handleAuth(req, res, db, bcrypt, jwt);
+});
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", auth.requireAuth, (req, res) => {
+  profile.handleProfileGet(req, res, db);
 });
 
 app.listen(3000, () => {

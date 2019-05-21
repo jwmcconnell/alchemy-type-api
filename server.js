0f7055db1a67b5dc666db0cecfaaeefe797f679a
Jackmc <jackmc1122@gmail.com>
2019-05-21 09:09:25 -0700

    make initial server.

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+// const knex = require("knex");
+// const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("alchemy type is working");
+});
+
+app.listen(3000, () => {
+    console.log(`running on port 3000`);
+});

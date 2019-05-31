85d41f6567d7507c3fb3cc29cebe88d7c069100d
Jackmc <jackmc1122@gmail.com>
2019-05-30 20:19:22 -0700

    Add back features and change connection string.

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const bcrypt = require("bcrypt-nodejs");
+const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
-// const knex = require("knex");
-// const jwt = require("jsonwebtoken");
+const knex = require("knex");
+const jwt = require("jsonwebtoken");
 const morgan = require("morgan");
 
-// const register = require("./controllers/register");
-// const login = require("./controllers/login");
-// const profile = require("./controllers/profile");
-// const auth = require("./middlewares/authorization");
+const register = require("./controllers/register");
+const login = require("./controllers/login");
+const profile = require("./controllers/profile");
+const auth = require("./middlewares/authorization");
 
-// const db = knex({
-//   client: "pg",
-//   connection: process.env.POSTGRES_URI
-// });
+const db = knex({
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  }
+});
 
 const port = process.env.PORT || 3000;
 
@@ -28,15 +31,15 @@ app.get("/", (req, res) => {
   res.send("alchemy type is working");
 });
 
-// app.post("/login", (req, res) => {
-//   login.handleAuth(req, res, db, bcrypt, jwt);
-// });
-// app.post("/register", (req, res) => {
-//   register.handleRegister(req, res, db, bcrypt);
-// });
-// app.get("/profile/:id", auth.requireAuth, (req, res) => {
-//   profile.handleProfileGet(req, res, db);
-// });
+app.post("/login", (req, res) => {
+  login.handleAuth(req, res, db, bcrypt, jwt);
+});
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", auth.requireAuth, (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
 
 app.listen(port, () => {
   console.log(`running on port ${port}`);

f46c27ac30738b56ded9df30b1cc6e8eab773dec
Jackmc <jackmc1122@gmail.com>
2019-05-30 20:41:25 -0700

    Update seed and add console log for test.

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,9 +1,9 @@
 BEGIN TRANSACTION;
 
 INSERT into users (name, email, joined) 
-values ('john', 'john@gmail.com', '2018-01-01');
+values ('test', 'test@gmail.com', '2018-01-01');
 INSERT into login (hash, email) 
-values ('$2a$10$0umiOSkXu//nmGjZxQZr9OEix8a89OjRRLA/PCFZJbOpN/juGpkAu', 'john@gmail.com');
+values ('$2a$10$0umiOSkXu//nmGjZxQZr9OEix8a89OjRRLA/PCFZJbOpN/juGpkAu', 'test@gmail.com');
 
 INSERT into passages (title, passage_text, passage_description, created) 
 values ('Javascript Functions', 'Practice typing Javascript functions!', 'This is a test passage.\n\nThis is a second line.', '2018-01-01');
